0e2f88dc1b958b2baf484abab51bd0d9f72f54cc
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-12-11 09:37:10 +0200

    That

---
+++ pages/images/index.vue
@@ -0,0 +1,220 @@
+<template>
+  <section class="bg-green-50 relative">
+    <img
+      src="/img/droite.png"
+      alt="Filigrane supérieur droit"
+      class="absolute top-80 right-0 w-40 pointer-events-none hidden sm:block"
+    />
+    <img
+      src="/img/gauche.png"
+      alt="Filigrane inférieur gauche"
+      class="absolute bottom-80 left-0 w-40 opacity-100 pointer-events-none hidden sm:block"
+    />
+
+    <div class="relative bg-custom-green p-8 mx-4 rounded-lg mt-20">
+      <!-- Image vectorielle (positionnée au-dessus du conteneur) -->
+      <img
+        src="/img/Vectoria.png"
+        alt="Decorative Vector"
+        class="absolute top-0 left-64 h-full w-auto z-10 hidden sm:block"
+      />
+      <div class="mj-container">
+        <div class="relative">
+          <div class="rounded-lg flex flex-col md:flex-row overflow-hidden">
+            <!-- Texte (à gauche sur grand écran) -->
+            <div class="flex-1 p-6 md:p-8">
+              <h1 class="text-4xl text-white font-bold mb-2">
+                {{ t("img.title_1") }}
+              </h1>
+              <nav class="text-sm text-black">
+                <div class="inline-block bg-white p-3 rounded-lg">
+                  <a href="/" class="hover:underline font-semibold">Home </a>
+                  /
+                  <span>{{ t("img.title_1") }}</span>
+                </div>
+              </nav>
+            </div>
+
+            <!-- Image (à droite sur grand écran) -->
+            <div class="flex-1 hidden md:flex">
+              <img
+                src="/img/t.jpg"
+                alt="About Us Section"
+                class="w-full h-full object-cover rounded-lg transition-all duration-300 hover:scale-105"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- contenu de l'equipe -->
+    <div class="mj-container my-4">
+      <div class="py-8 px-4">
+        <!-- Titre Principal -->
+        <div class="text-center my-6">
+          <p class="text-custom-green uppercase tracking-widest text-xs">
+            {{ t("img.title_1") }}
+          </p>
+          <h1 class="text-3xl font-semibold">
+            {{ t("team.title_2") }}
+            <span class="font-sri"> {{ t("img.title_1") }}</span>
+          </h1>
+        </div>
+
+        <!-- Navigation des catégories -->
+        <div
+          class="flex justify-center space-x-4 mb-8 flex-wrap md:flex-nowrap"
+        >
+          <button
+            v-for="(category, index) in categories"
+            :key="index"
+            :class="[
+              'px-4 py-2 text-lg font-medium border-b-2',
+              activeCategory === category
+                ? 'border-custom-green text-custom-green'
+                : 'border-transparent text-gray-600 hover:text-gray-800',
+              // Taille et espacement adaptatifs
+              'sm:text-base sm:px-3 sm:py-1 md:text-lg md:px-4 md:py-2',
+            ]"
+            @click="setActiveCategory(category)"
+          >
+            {{ category }}
+          </button>
+        </div>
+
+        <!-- Section : Management -->
+        <div
+          v-show="activeCategory === t('team.btn_1')"
+          class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6"
+        >
+          <div
+            v-for="(member, index) in team"
+            :key="index"
+            class="relative group bg-white shadow-md rounded-lg overflow-hidden hover:bg-custom-green transition"
+          >
+            <!-- Effet de Hover -->
+            <div
+              class="absolute inset-0 bg-custom-green hover:border-white opacity-0 group-hover:opacity-90 transition"
+            ></div>
+
+            <!-- Contenu des cartes -->
+            <div class="relative p-4 text-center group-hover:text-white z-10">
+              <img
+                :src="member.image"
+                :alt="member.name"
+                class="w-16 h-16 mx-auto rounded-full mb-4 border-2 border-custom-green hover:border-white"
+              />
+              <h2 class="text-lg font-semibold">{{ member.name }}</h2>
+              <p class="text-sm">{{ member.role }}</p>
+
+              <!-- Icônes -->
+              <!-- Icônes -->
+              <div class="flex justify-center space-x-2 mt-4">
+                <a
+                  :href="member.social.linkedin"
+                  target="_blank"
+                  class="w-8 h-8 bg-green-200 rounded-full flex items-center justify-center text-custom-green hover:bg-white hover:text-custom-green transition"
+                >
+                  <i class="fab fa-linkedin"></i>
+                </a>
+                <a
+                  :href="member.social.twitter"
+                  target="_blank"
+                  class="w-8 h-8 bg-green-200 rounded-full flex items-center justify-center text-custom-green hover:bg-white hover:text-custom-green transition"
+                >
+                  <i class="fab fa-x-twitter"></i>
+                </a>
+                <a
+                  :href="member.social.facebook"
+                  target="_blank"
+                  class="w-8 h-8 bg-green-200 rounded-full flex items-center justify-center text-custom-green hover:bg-white hover:text-custom-green transition"
+                >
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Section : Board of Direction -->
+        <div
+          v-show="activeCategory === t('team.btn_2')"
+          class="text-center text-gray-600"
+        >
+          <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <div
+              v-for="(member, index) in board"
+              :key="index"
+              class="relative group bg-white shadow-md rounded-lg overflow-hidden hover:bg-custom-green transition"
+            >
+              <!-- Effet de Hover -->
+              <div
+                class="absolute inset-0 bg-custom-green opacity-0 group-hover:opacity-90 transition"
+              ></div>
+
+              <!-- Contenu des cartes -->
+              <div class="relative p-4 text-center group-hover:text-white z-10">
+                <img
+                  :src="member.image"
+                  :alt="member.name"
+                  class="w-16 h-16 mx-auto rounded-full mb-4 border-2 border-custom-green hover:borger-white"
+                />
+                <h2 class="text-lg font-semibold">{{ member.name }}</h2>
+                <p class="text-sm">{{ member.role }}</p>
+
+                <!-- Icônes -->
+                <!-- Icônes -->
+                <div class="flex justify-center space-x-2 mt-4">
+                  <a
+                    :href="member.social.linkedin"
+                    target="_blank"
+                    class="w-8 h-8 bg-green-200 rounded-full flex items-center justify-center text-custom-green hover:bg-white hover:text-custom-green transition"
+                  >
+                    <i class="fab fa-linkedin"></i>
+                  </a>
+                  <a
+                    :href="member.social.twitter"
+                    target="_blank"
+                    class="w-8 h-8 bg-green-200 rounded-full flex items-center justify-center text-custom-green hover:bg-white hover:text-custom-green transition"
+                  >
+                    <i class="fab fa-x-twitter"></i>
+                  </a>
+                  <a
+                    :href="member.social.facebook"
+                    target="_blank"
+                    class="w-8 h-8 bg-green-200 rounded-full flex items-center justify-center text-custom-green hover:bg-white hover:text-custom-green transition"
+                  >
+                    <i class="fab fa-facebook-f"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Section : Financial Repport -->
+        <div
+          v-show="activeCategory === t('team.btn_3')"
+          class="text-center text-gray-600"
+        >
+          <all />
+        </div>
+      </div>
+    </div>
+    <!-- gallerie ici -->
+    <div class="mj-container"></div>
+    <hr class="mj-container" />
+    <!-- Parteners -->
+    <div class="my-16">
+      <LogOurs />
+    </div>
+    <!-- Footer -->
+    <div class="mt-16">
+      <laster />
+    </div>
+  </section>
+</template>
+<script setup>
+const { t } = useI18n();
+// Les catégories
+</script>
