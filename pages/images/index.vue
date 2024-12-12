772d43d341d45544a19fd61557f4907657a19327
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-12-12 11:54:20 +0200

    ThatHere

--- pages/images/index.vue
+++ pages/images/index.vue
@@ -206,23 +206,23 @@
       <h1 class="text-4xl font-bold text-center mb-6">
         {{ t("img.title_3") }}
       </h1>
-      <div
-        class="grid grid-cols-2 gap-4 md:flex md:justify-center md:space-x-4 mb-8 mt-8"
-      >
         <div
-          v-for="country in countries"
-          :key="country.name"
-          :class="{
-            'flex items-center justify-center space-x-2 cursor-pointer px-4 py-2 w-full md:w-auto': true,
-            'bg-custom-green text-white': activeCountry === country.name,
-            'bg-white border border-gray-300': activeCountry !== country.name,
-          }"
-          @click="selectCountry(country.name)"
+          class="grid grid-cols-2 gap-4 md:flex md:justify-center md:space-x-4 mb-8 mt-8"
         >
-          <NuxtImg :src="country.flag" :alt="country.name" class="w-6 h-4" />
-          <span class="font-medium">{{ country.name }}</span>
+          <div
+            v-for="country in countries"
+            :key="country.name"
+            :class="{
+              'flex items-center justify-center space-x-2 cursor-pointer px-4 py-2 w-full md:w-auto': true,
+              'bg-custom-green text-white': activeCountry === country.name,
+              'bg-white border border-gray-300': activeCountry !== country.name,
+            }"
+            @click="selectCountry(country.name)"
+          >
+            <NuxtImg :src="country.flag" :alt="country.name" class="w-6 h-4" />
+            <span class="font-medium">{{ country.name }}</span>
+          </div>
         </div>
-      </div>
 
       <!-- img debut -->
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
