d17d6be787e39c9ce843df3f32de33e432b8d0c1
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2024-12-23 14:43:13 +0200

    ThatsGreat

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ export default {
 
   theme: {
     extend: {
+      animation: {
+        marquee: "marquee 20s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
       colors: {
         customLight: "#F9F9F4",
         "custom-greens": "#07B008",
