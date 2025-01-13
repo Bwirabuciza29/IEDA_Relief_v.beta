0bdeaa7f69658d2bdce92a04da4993c74a16e149
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2025-01-13 12:49:16 +0200

    greaat

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="flex flex-col items-center justify-center mt-56">
       <img src="/img/chargement.gif" alt="Chargement" class="h-48 w-48 mb-2" />
-      <p class="text-gray-600 text-center text-lg">Under Construction</p>
+      <p class="text-gray-600 text-center">Under Construction</p>
     </div>
   </div>
 </template>
