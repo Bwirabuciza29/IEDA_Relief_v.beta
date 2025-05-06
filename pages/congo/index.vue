0242337c593ab7da0e7dc5776090b1b20cadb8c1
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2025-05-06 13:42:04 +0200

    HereWeAre

---
+++ pages/congo/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mt-36 mj-container">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae
+    deleniti aliquam hic quis totam harum tempore impedit sequi pariatur. Sequi
+    corporis eius voluptates autem deserunt officiis nostrum sunt delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae
+    deleniti aliquam hic quis totam harum tempore impedit sequi pariatur. Sequi
+    corporis eius voluptates autem deserunt officiis nostrum sunt delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae
+    deleniti aliquam hic quis totam harum tempore impedit sequi pariatur. Sequi
+    corporis eius voluptates autem deserunt officiis nostrum sunt delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae
+    deleniti aliquam hic quis totam harum tempore impedit sequi pariatur. Sequi
+    corporis eius voluptates autem deserunt officiis nostrum sunt delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae
+    deleniti aliquam hic quis totam harum tempore impedit sequi pariatur. Sequi
+    corporis eius voluptates autem deserunt officiis nostrum sunt delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae
+    deleniti aliquam hic quis totam harum tempore impedit sequi pariatur. Sequi
+    corporis eius voluptates autem deserunt officiis nostrum sunt delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae
+    deleniti aliquam hic quis totam harum tempore impedit sequi pariatur. Sequi
+    corporis eius voluptates autem deserunt officiis nostrum sunt delectus.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam beatae
+    deleniti aliquam hic quis totam harum tempore impedit sequi pariatur. Sequi
+    corporis eius voluptates autem deserunt officiis nostrum sunt delectus.
+  </div>
+</template>
