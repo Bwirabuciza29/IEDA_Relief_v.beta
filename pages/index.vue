644588b82eeb420493265f5c3f7996cf291e9ee1
Bwirabuciza29 <blondybwirabuciza@gmail.com>
2025-07-14 11:45:46 +0200

    hereWeUpdates

--- pages/index.vue
+++ pages/index.vue
@@ -1,138 +1,126 @@
 <template>
   <div class="bg-green-50">
     <div
-  v-if="isLoadingPays"
-  class="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-white/70 transition-opacity duration-300"
->
-<div class="text-center">
-  <img src="/img/bg/lol.png" alt="Chargement" class="w-auto h-24 animate-bounce mb-4 mx-auto bg-white p-2  border-t border-b border-custom-green" />
-  <p class="font-semibold text-custom-green">Chargement en cours...</p>
-</div>
-</div>
+      v-if="isLoadingPays"
+      class="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-white/70 transition-opacity duration-300"
+    >
+      <div class="text-center">
+        <img
+          src="/img/bg/lol.png"
+          alt="Chargement"
+          class="w-auto h-24 animate-bounce mb-4 mx-auto bg-white p-2 border-t border-b border-custom-green"
+        />
+        <p class="font-semibold text-custom-green">Chargement en cours...</p>
+      </div>
+    </div>
     <div v-if="selectedPays === 'All'">
-    <div  class="mj-container relative mt-20">
-      <div class="relative z-10">
-        <div
-          class="flex flex-col md:flex-row items-center md:justify-between py-10"
-        >
-          <div class="md:w-1/2 space-y-6 text-center md:text-left">
-            <h1 class="text-4xl md:text-5xl font-semibold">
-              {{ title_1 }}
-              <span class="text-custom-green font-sri"> {{ title_2 }} </span>
-              {{ title_3 }}
-              <span class="text-custom-green font-sri"> {{ title_4 }} </span>...
-            </h1>
-            <p class="text-gray-700 text-lg text-justify">
-              {{ desc }}
-            </p>
-            <NuxtLink
-              :to="localPagePath('donation')"
-              class="group relative inline-block w-full sm:w-auto overflow-hidden border-2 border-green-100 bg-custom-green px-6 py-4 text-sm font-semibold text-white sm:px-6 sm:py-3 sm:text-base"
-            >
-              <span
-                class="relative z-10 translate-x-full transform transition-all duration-300 ease-in-out group-hover:translate-x-0 group-hover:text-green-700"
+      <div class="mj-container relative mt-20">
+        <div class="relative z-10">
+          <div class="flex flex-col md:flex-row items-center md:justify-between py-10">
+            <div class="md:w-1/2 space-y-6 text-center md:text-left">
+              <h1 class="text-4xl md:text-5xl font-semibold">
+                {{ title_1 }}
+                <span class="text-custom-green font-sri"> {{ title_2 }} </span>
+                {{ title_3 }}
+                <span class="text-custom-green font-sri"> {{ title_4 }} </span>...
+              </h1>
+              <p class="text-gray-700 text-lg text-justify">
+                {{ desc }}
+              </p>
+              <NuxtLink
+                :to="localPagePath('donation')"
+                class="group relative inline-block w-full sm:w-auto overflow-hidden border-2 border-green-100 bg-custom-green px-6 py-4 text-sm font-semibold text-white sm:px-6 sm:py-3 sm:text-base"
               >
-                {{ btn }}
-              </span>
-              <div
-                class="absolute inset-0 h-full w-full translate-x-full transform bg-green-100 transition-transform duration-300 ease-in-out group-hover:translate-x-0"
-              ></div>
-            </NuxtLink>
-          </div>
-
-          <div
-            class="md:w-1/2 flex flex-wrap md:justify-end gap-4 mt-10 md:mt-0 relative"
-          >
-            <div class="relative md:w-3/4">
-              <img
-                src="/img/hero.jpg"
-                alt="Community support"
-                class="transform rounded-lg"
-              />
-              <img
-                src="/img/img_1.png"
-                alt="Volunteer"
-                class="hidden md:block mx-2 absolute top-[50%] left-[-40px] w-20 h-20 md:w-28 md:h-28 shadow-md rounded-lg transition-all duration-300 hover:z-10 hover:scale-110"
-              />
-              <img
-                src="/img/img_2.png"
-                alt="Child"
-                class="hidden md:block mx-2 absolute top-[20%] right-[-40px] w-20 h-20 md:w-28 md:h-28 shadow-md transform rounded-lg transition-all duration-300 hover:z-10 hover:scale-110"
-              />
+                <span
+                  class="relative z-10 translate-x-full transform transition-all duration-300 ease-in-out group-hover:translate-x-0 group-hover:text-green-700"
+                >
+                  {{ btn }}
+                </span>
+                <div
+                  class="absolute inset-0 h-full w-full translate-x-full transform bg-green-100 transition-transform duration-300 ease-in-out group-hover:translate-x-0"
+                ></div>
+              </NuxtLink>
             </div>
+
             <div
-              class="bg-custom-green py-6 rounded-md flex items-center justify-center w-2/4 md:w-2/2 absolute -bottom-16 left-44 transform -translate-x-1/2 md:translate-x-0 transition hover:scale-105 mobile:bottom-8 mobile:left-1/2 mobile:transform mobile:-translate-x-1/2"
+              class="md:w-1/2 flex flex-wrap md:justify-end gap-4 mt-10 md:mt-0 relative"
             >
-              <div class="text-center">
-                <p class="text-2xl md:text-3xl text-white font-sri">
-                  {{ animatedCardNum }} {{ card_letter }}
-                </p>
-                <p class="text-white hover:underline">
-                  {{ card_desc }}
-                </p>
+              <div class="relative md:w-3/4">
+                <img
+                  src="/img/hero.jpg"
+                  alt="Community support"
+                  class="transform rounded-lg"
+                />
+                <img
+                  src="/img/img_1.png"
+                  alt="Volunteer"
+                  class="hidden md:block mx-2 absolute top-[50%] left-[-40px] w-20 h-20 md:w-28 md:h-28 shadow-md rounded-lg transition-all duration-300 hover:z-10 hover:scale-110"
+                />
+                <img
+                  src="/img/img_2.png"
+                  alt="Child"
+                  class="hidden md:block mx-2 absolute top-[20%] right-[-40px] w-20 h-20 md:w-28 md:h-28 shadow-md transform rounded-lg transition-all duration-300 hover:z-10 hover:scale-110"
+                />
+              </div>
+              <div
+                class="bg-custom-green py-6 rounded-md flex items-center justify-center w-2/4 md:w-2/2 absolute -bottom-16 left-44 transform -translate-x-1/2 md:translate-x-0 transition hover:scale-105 mobile:bottom-8 mobile:left-1/2 mobile:transform mobile:-translate-x-1/2"
+              >
+                <div class="text-center">
+                  <p class="text-2xl md:text-3xl text-white font-sri">
+                    {{ animatedCardNum }} {{ card_letter }}
+                  </p>
+                  <p class="text-white hover:underline">
+                    {{ card_desc }}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <img
+          src="/img/Vector_1.png"
+          alt="Filigrane"
+          class="absolute right-0 top-48 transform -translate-y-1/2 opacity-90 pointer-events-none w-64 sm:w-96 md:w-[25rem] lg:w-[35rem] h-[20rem] sm:h-[25rem] md:h-[30rem] lg:h-[35rem] z-0"
+        />
+        <img
+          src="/img/Vector_1.png"
+          alt="Filigrane"
+          class="absolute left-0 -bottom-48 transform -translate-x-1/2 opacity-50 pointer-events-none w-64 sm:w-96 md:w-[25rem] lg:w-[35rem] h-[20rem] sm:h-[25rem] md:h-[30rem] lg:h-[35rem] z-0"
+        />
       </div>
-      <img
-        src="/img/Vector_1.png"
-        alt="Filigrane"
-        class="absolute right-0 top-48 transform -translate-y-1/2 opacity-90 pointer-events-none w-64 sm:w-96 md:w-[25rem] lg:w-[35rem] h-[20rem] sm:h-[25rem] md:h-[30rem] lg:h-[35rem] z-0"
-      />
-      <img
-        src="/img/Vector_1.png"
-        alt="Filigrane"
-        class="absolute left-0 -bottom-48 transform -translate-x-1/2 opacity-50 pointer-events-none w-64 sm:w-96 md:w-[25rem] lg:w-[35rem] h-[20rem] sm:h-[25rem] md:h-[30rem] lg:h-[35rem] z-0"
-      />
-    </div>
-    <!-- Si c'est USA -->
-    <!-- Fin Page d'accueil -->
-    <div class="my-16 bg-white p-4 border-t border-b border-custom-green">
-      <log />
-    </div>
-    <div class="mt-16" data-aos="fade-up">
-      <mission />
-    </div>
-    <div>
-      <actualite />
-    </div>
+      <!-- Si c'est USA -->
+      <!-- Fin Page d'accueil -->
+      <div class="my-16 bg-white p-4 border-t border-b border-custom-green">
+        <log />
+      </div>
+      <div class="mt-16" data-aos="fade-up">
+        <mission />
+      </div>
+      <div>
+        <actualite />
+      </div>
 
-    <div class="my-16" data-aos="fade-up">
-      <help />
+      <div class="my-16" data-aos="fade-up">
+        <help />
+      </div>
+      <div class="my-16" data-aos="fade-up">
+        <parteners />
+      </div>
     </div>
-    <div class="my-16" data-aos="fade-up">
-      <parteners />
+    <div v-else-if="selectedPays === 'USA'" class="relative mt-20">
+      <usa />
     </div>
+    <div v-else-if="selectedPays === 'Burkina Faso'" class="relative mt-20">
+      <burkina />
     </div>
-    <div v-else-if="selectedPays === 'USA'" class=" relative mt-20">
-         <usa  />
+    <div v-else-if="selectedPays === 'CAR'" class="relative mt-20">
+      <car />
     </div>
-    <div
-      v-else-if="selectedPays === 'Burkina Faso'"
-      class=" relative mt-20"
-    >    <burkina />
-    </div>
-    <div v-else-if="selectedPays === 'CAR'" class=" relative mt-20">
-         <car />
-    </div>
-    <div
-      v-else-if="selectedPays === 'Niger'"
-      class=" relative mt-20"
-    ><niger />
-    </div>
-    <div
-      v-else-if="selectedPays === 'Mali'"
-      class=" relative mt-20"
-    > <mali />
-    </div>
-      <div
-      v-else-if="selectedPays === 'Cameroun'"
-      class=" relative mt-20"
-    > <cameroun />
-    </div>
+    <div v-else-if="selectedPays === 'Niger'" class="relative mt-20"><niger /></div>
+    <div v-else-if="selectedPays === 'Mali'" class="relative mt-20"><mali /></div>
+    <div v-else-if="selectedPays === 'Cameroun'" class="relative mt-20"><cameroun /></div>
     <div v-else-if="selectedPays === 'DRC'" class="relative mt-20">
-     <congo />
+      <congo />
     </div>
 
     <div class="my-16">
@@ -159,8 +147,7 @@ const { t, locale } = useI18n();
 import { usePaysStore } from "/stores/usePaysStore.js";
 import { storeToRefs } from "pinia";
 const paysStore = usePaysStore();
-const { selectedPays, paysList, isLoading, dropdownOpen } =
-  storeToRefs(paysStore);
+const { selectedPays, paysList, isLoading, dropdownOpen } = storeToRefs(paysStore);
 const { fetchPays, selectPays, toggleDropdown } = paysStore;
 const title_1 = ref(t("hero.title_1"));
 const title_2 = ref(t("hero.title_2"));
